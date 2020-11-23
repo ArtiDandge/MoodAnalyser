f1f41c4cc4b74fa55f0d9c98c8ced467ab6f8660
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-11-23 10:39:11 +0530

    Throw exception when invalid mood it encounted

--- src/com/moodanalyser/main/MoodAnalyser.java
+++ src/com/moodanalyser/main/MoodAnalyser.java
@@ -1,14 +1,17 @@
 package com.moodanalyser.main;
 
 public class MoodAnalyser {
-	public String analyseMood(String mood) {
+	public String analyseMood(String mood) throws MoodAnalyserException {
 		// TODO Auto-generated method stub
+		try {
 			if (mood.equals("SAD")) { 
 				return "SAD";
 			}
 			else 
 			return mood;
-			
+		}catch(NullPointerException e) {
+			throw new MoodAnalyserException("Invalid Mood");
+		}	
 	}
 				
 
--- src/com/moodanalyser/main/MoodAnalyserTest.java
+++ src/com/moodanalyser/main/MoodAnalyserTest.java
@@ -10,15 +10,25 @@ class MoodAnalyserTest {
 	MoodAnalyser moodAnalyser = new MoodAnalyser();
 	
 	@Test
-	public void givenSad_WhenAnalaylseMood_ThenShouldReturnSAD() {
+	public void givenSad_WhenAnalaylseMood_ThenShouldReturnSAD() throws MoodAnalyserException {
 		Assert.assertEquals("I am in SAD mood",moodAnalyser.analyseMood("I am in SAD mood"));
 		
 	}
 	
 	@Test
-	public void givenHappy_WhenAnalaylseMood_ThenShouldReturnHAPPY() {
+	public void givenHappy_WhenAnalaylseMood_ThenShouldReturnHAPPY() throws MoodAnalyserException {
 		Assert.assertEquals("I am in HAPPY mood",moodAnalyser.analyseMood("I am in HAPPY mood"));
 		
+	}
+	
+	@Test
+	public void givenNullMood_ReturnHappy() {
+		try {
+			Assert.assertEquals("Happy mood", moodAnalyser.analyseMood(null));
+		} catch (MoodAnalyserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
